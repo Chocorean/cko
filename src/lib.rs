ee1b05726cf2b2413f6f4a741f542f472dec1227
Baptiste CHOCOT <baptiste.chocot@nist.gov>
2023-05-17 15:54:40 -0400

    add pointing_outside test impl

--- src/lib.rs
+++ src/lib.rs
@@ -25,19 +25,30 @@ pub fn inspect(path: &str) -> Option<Vec<String>> {
         }
         // check destination of soft links
         else if filetype.is_symlink() {
-            let entry_path = entry.path();
-            let dest_path = read_link(entry_path).unwrap();
-            println!("\tcurrent {}\n\tentry {}\n\tdest {}", current_path.display(), entry_path.display(), dest_path.display());
+            let full_entry_path = entry.path();
+            let entry_path = full_entry_path.strip_prefix(current_path.clone()).unwrap();
+            let dest_path = read_link(full_entry_path).unwrap();
+            println!("\tcurrent {}\n\tentry {}\n\tdest {}", current_path.display(), full_entry_path.display(), dest_path.display());
 
             // make dest_path absolute and canocical
             let mut full_dest_path = if ! dest_path.to_str().unwrap().starts_with("/") {
-                entry_path.parent().unwrap().join(dest_path.clone())
+                full_entry_path.parent().unwrap().join(dest_path.clone())
             } else {
                 dest_path.clone()
             };
             println!("debug: symlink {}", full_dest_path.display());
-            full_dest_path = canonicalize(full_dest_path).unwrap();
-            println!("debug: caconical symlink {}", full_dest_path.display());
+            let result = canonicalize(full_dest_path.clone());
+            match result {
+                Ok(p) => {
+                    full_dest_path = p;
+                    println!("debug: caconical symlink {}", full_dest_path.display());
+                },
+                Err(_) => {
+                    msg.push(format!("{} is pointing outside to {}", entry_path.display(), dest_path.display()));
+                    continue;
+                }
+            };
+            
 
             // check if dest is inside the dir we are exploring
             if ! full_dest_path.starts_with(&current_path) {
@@ -82,31 +93,31 @@ mod test {
 
     #[test]
     fn test_inspect_pointing_outside() {
-        assert_eq!(inspect("data/pointing_outside"), Some(vec!["data/pointing_outside/link is pointing outside to ../valid_dir".to_string()]));
+        assert_eq!(inspect("data/pointing_outside"), Some(vec!["link is pointing outside to ../valid_dir".to_string()]));
     }
 
     #[test]
     fn test_inspect_loop() {
-        assert_eq!(inspect("data/loop"), Some(vec!["data/loop/link0".to_string()]));
+        assert_eq!(inspect("data/loop"), Some(vec!["link0".to_string()]));
     }
 
     #[test]
     fn test_inspect_hardlink() {
-        assert_eq!(inspect("data/hardlink").unwrap().sort(), vec!["data/hardlink/file has more (2) than one hard link".to_string(), "data/hardlink/hardlink has more (2) than one hard link".to_string()].sort());
+        assert_eq!(inspect("data/hardlink").unwrap().sort(), vec!["file has more (2) than one hard link".to_string(), "hardlink has more (2) than one hard link".to_string()].sort());
     }
 
     #[test]
     fn test_inspect_broken_softlink() {
-        assert_eq!(inspect("data/broken_softlink"), Some(vec!["data/broken_softlink/link".to_string()]));
+        assert_eq!(inspect("data/broken_softlink"), Some(vec!["link".to_string()]));
     }
 
     #[test]
     fn test_inspect_absolute_softlink() {
-        assert_eq!(inspect("data/absolute_softlink"), Some(vec!["data/absolute_softlink/link".to_string()]));
+        assert_eq!(inspect("data/absolute_softlink"), Some(vec!["link".to_string()]));
     }
 
     #[test]
     fn test_inspect_many_issues() {
-        assert_eq!(inspect("data/many_issues"), Some(vec!["data/many_issues/absolute".to_string(), "data/many_issues/broken".to_string(), "data/many_issues/loop".to_string()]));
+        assert_eq!(inspect("data/many_issues"), Some(vec!["absolute".to_string(), "broken".to_string(), "loop".to_string()]));
     }
 }
