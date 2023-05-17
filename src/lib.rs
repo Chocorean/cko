2b929ce5db61afec50b889a172709af032a7bcfe
Baptiste CHOCOT <baptiste.chocot@nist.gov>
2023-05-17 15:40:17 -0400

    always build absolute paths

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,66 @@
-use std::{fs::{read_link, symlink_metadata}, os::unix::prelude::MetadataExt};
+use std::env::current_dir;
+use std::path::PathBuf;
+use std::{fs::{canonicalize, read_link, symlink_metadata}, os::unix::prelude::MetadataExt};
 use walkdir::WalkDir;
+
 
 /// Recursively inspect a directory and return a list of incorrect files.
 pub fn inspect(path: &str) -> Option<Vec<String>> {
+    let current_path = if ! path.starts_with("/") {
+        canonicalize(current_dir().unwrap().join(path)).unwrap()
+    } else {
+        let mut p = PathBuf::new();
+        p.push(path);
+        p
+    };
     let mut msg = vec![];
 
     // Skipping possible issues like permissions related
-    for entry in WalkDir::new(&path).into_iter().filter_map(|e| e.ok()) {
+    for entry in WalkDir::new(&current_path).into_iter().filter_map(|e| e.ok()) {
+        println!("debug: inspecting {}", entry.file_name().to_str().unwrap());
         let filetype = entry.file_type();
         // ignoring dicts
         if filetype.is_dir() {
             continue;
         }
         // check destination of soft links
-        if filetype.is_symlink() {
+        else if filetype.is_symlink() {
             let entry_path = entry.path();
-            let dest = read_link(entry_path).unwrap();
-            // check if dest is inside 
-            println!("{} -> {}", entry_path.display(), dest.display());
+            let dest_path = read_link(entry_path).unwrap();
+            println!("\tcurrent {}\n\tentry {}\n\tdest {}", current_path.display(), entry_path.display(), dest_path.display());
+
+            // make dest_path absolute and canocical
+            let mut full_dest_path = if ! dest_path.to_str().unwrap().starts_with("/") {
+                entry_path.parent().unwrap().join(dest_path.clone())
+            } else {
+                dest_path.clone()
+            };
+            println!("debug: symlink {}", full_dest_path.display());
+            full_dest_path = canonicalize(full_dest_path).unwrap();
+            println!("debug: caconical symlink {}", full_dest_path.display());
+
+            // check if dest is inside the dir we are exploring
+            if ! full_dest_path.starts_with(&current_path) {
+                msg.push(format!("{} is pointing outsite to {}", entry_path.display(), dest_path.display()));
+                continue;
+            }
+
+            // then, check if the file actually exists
+
+            // then, check if it does not loop
         }
         // if file, check number of inode stuff
-        if filetype.is_file() {
+        else if filetype.is_file() {
             let entry_path = entry.path();
             let md = symlink_metadata(entry_path).unwrap();
             let nlink = md.nlink();
             if nlink != 1 {
                 msg.push(format!("{} has more ({}) than one hard link", entry_path.display(), nlink));
             }
+        }
+        // special files I guess ?
+        else {
+            msg.push(format!("WARING: unknown file type: {}", entry.path().display()));
         }
     }
 
@@ -48,7 +82,7 @@ mod test {
 
     #[test]
     fn test_inspect_pointing_outside() {
-        assert_eq!(inspect("data/pointing_outside"), Some(vec!["data/pointing_outside/link".to_string()]));
+        assert_eq!(inspect("data/pointing_outside"), Some(vec!["data/pointing_outside/link is pointing outside to ../valid_dir".to_string()]));
     }
 
     #[test]
